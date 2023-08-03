0df28b4ed58d8573549c8c9e80e3013031ef0e8b
Bartek <b.ciesliczka@gmail.com>
2023-08-03 23:35:12 +0200

    fixing pic bug

--- src/components/PokemonsList/Tile/Tile.js
+++ src/components/PokemonsList/Tile/Tile.js
@@ -1,10 +1,17 @@
+import { padding } from "@mui/system";
 import "./Tile.css";
 
 export function Tile({ pokemonData }) {
   return (
     <div className="tile">
       <div className="tile__imgContainer">
-        <img className="tile__img" src={pokemonData.sprites.front_default} />
+        {pokemonData.sprites.front_default ? (
+          <img className="tile__img" src={pokemonData.sprites.front_default} />
+        ) : (
+          <div style={{ textAlign: "center", padding: "85px 0" }}>
+            Pic not found
+          </div>
+        )}
       </div>
       <p className="tile__name">{pokemonData.name}</p>
       <p className="tile__ID">#{pokemonData.id}</p>
