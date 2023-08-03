f5e417aed195a814238c4859d473181954fd42d5
Bartek <b.ciesliczka@gmail.com>
2023-08-03 13:46:53 +0200

    changed background-color in app.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Searchbar } from "./components/SearchPokemon/Searchbar/Searchbar";
 import { ListLayout } from "./components/PokemonsList/ListLayout/ListLayout";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <main>
+      <div className="headerr"></div>
       <div className="pokedex">
         <h1 className="pokedex__title">POKEDEX</h1>
         <Searchbar />
